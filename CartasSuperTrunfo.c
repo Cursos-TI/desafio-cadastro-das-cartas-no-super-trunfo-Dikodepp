af05432c4c1bd45846fd00d7b3ccd6e9d597b517
Dikodepp <dikodepp@gmail.com>
2025-02-18 22:14:25 +0000

    organizaçao do codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,26 +4,24 @@ int main(){
 
     //declarando o nome das variaveis
     char estado;
+    char codigodacarta[5];
     char nomedacidade[100];
-    char codigodacarta[100];
     int populacao;
     int Numeropontosturisticos;
     float pib;
     float areadacidade;
 
     //entrada do usuário para cadastro de cartas
-    printf("Digite o estado:\n");
+    printf("Digite uma letra de A a H que corresponde ao estado: \n");
     scanf("%c", &estado);
     getchar();
 
-    printf("Digite o nome da cidade: \n"); 
-    scanf("%[^\n]", &nomedacidade);
-
     printf("Digite o codigo da carta: \n");
     scanf( "%s" , &codigodacarta);  //armazenado nas variaveis
+    getchar();
 
-    printf("Digite o número do PIB da cidade: \n");
-    scanf("%f", &pib);  //armazenado nas variaveis
+    printf("Digite o nome da cidade: \n"); 
+    scanf("%[^\n]", nomedacidade);
 
     printf("Digite a população da cidade: \n");
     scanf("%d", &populacao);  //armazenado nas variaveis
@@ -32,17 +30,20 @@ int main(){
     printf("Digite quantos metros quadrados tem a cidade: \n");
     scanf("%f",&areadacidade);
 
+    printf("Digite o número do PIB da cidade: \n");
+    scanf("%f", &pib);  //armazenado nas variaveis
+
     printf("Digite o números de pontos turísticos: \n");
     scanf("%d", &Numeropontosturisticos);  //armazenado nas variaveis
 
     //impressão dos dados cadastrados pelo usuário
     printf("Estado: %c\n", estado);
+    printf("Código: %s\n" , codigodacarta);
     printf("Nome da cidade: %s\n" , nomedacidade);
-    printf("Codigo da carta: %s\n" , codigodacarta);
-    printf("Número do PIB: R$ %.2f bilhões de reais \n" , pib);
-    printf("Número da população: %d \n", populacao);
+    printf("Número da população: %d milhões\n", populacao);
     printf("extensão da cidade: %.2f km²\n", areadacidade);
-    printf("Numero de pontos turisticos:%d " , Numeropontosturisticos);
+    printf("Número do PIB: R$ %.2f bilhões de reais \n" , pib);
+    printf("Numero de pontos turisticos: %d " , Numeropontosturisticos);
 
     return 0;
 
